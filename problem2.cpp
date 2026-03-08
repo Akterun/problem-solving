d1d657482f99d08a4e050e974906c8746e487573
Akterun <akteruneti@gmail.com>
2026-03-08 21:28:22 +0600

    problem 2

---
+++ problem2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    long long k;
+    cin >> n >> m >> k;
+
+    long long a[n], b[m];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    sort(a, a + n);
+    sort(b, b + m);
+
+    int i = 0, j = 0;
+    int count = 0;
+
+
+    while (i < n && j < m) {
+        if (b[j] < a[i] - k) {
+            j++;
+        } else if (b[j] > a[i] + k) {
+            i++;
+        } else {
+
+            count++;
+            i++;
+            j++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
+
